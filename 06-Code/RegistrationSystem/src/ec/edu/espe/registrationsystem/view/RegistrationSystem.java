75c02584b30d859b48a1946ab21cb2361dac93c0
adarboleda <adarboleda@espe.edu.ec>
2024-06-02 11:33:38 -0500

    arboleda

---
+++ 06-Code/RegistrationSystem/src/ec/edu/espe/registrationsystem/view/RegistrationSystem.java
@@ -0,0 +1,16 @@
+package ec.edu.espe.registrationsystem.view;
+
+/**
+ *
+ * @author Abner Arboleda, Logic Legion, DCCO-ESPE
+ */
+public class RegistrationSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
