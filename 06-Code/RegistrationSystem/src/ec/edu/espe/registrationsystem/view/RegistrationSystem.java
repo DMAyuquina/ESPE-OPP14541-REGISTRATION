cc825193fd046de2daa4f80c9c76f5e927b79fb5
EnzoAlbertoArmijosMacas <armijosenzo@gmail.com>
2024-06-06 10:52:25 -0500

    RS V0.13

--- 06-Code/RegistrationSystem/src/ec/edu/espe/registrationsystem/view/RegistrationSystem.java
+++ 06-Code/RegistrationSystem/src/ec/edu/espe/registrationsystem/view/RegistrationSystem.java
@@ -9,7 +9,7 @@ import java.awt.Menu;
 public class RegistrationSystem {
 
     public static void main(String[] args) {
-        System.out.println("Sistema de Matriculas de Estidiantes Intituto Tecnologico Superior Bolivar V0.12");
+        System.out.println("--------Sistema de Matriculas de Estidiantes Intituto Tecnologico Superior Bolivar V0.13-------");
 
         ManagerMenu objetoMenu = new ManagerMenu();
         objetoMenu.callMainMenu();
