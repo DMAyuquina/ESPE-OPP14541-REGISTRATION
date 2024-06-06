0fa74f6fc534579967e6be4fdbc3f23489668c1c
cmacuna7 <crissre456@hotmail.com>
2024-06-05 20:24:34 -0500

    RV

--- 06-Code/RegistrationSystem/src/ec/edu/espe/registrationsystem/view/RegistrationSystem.java
+++ 06-Code/RegistrationSystem/src/ec/edu/espe/registrationsystem/view/RegistrationSystem.java
@@ -1,33 +1,19 @@
-//package ec.edu.espe.registrationsystem.view;
-//
-//import java.awt.Menu;
-//
-//
-//
-///**
-// *
-// * @author Marcelo Acuña, Logic Legion, DCCO-ESPE
-// */
-//public class RegistrationSystem {
-//<<<<<<< HEAD
-//
-//    public static void main(String[] args) {
-//
-//        System.out.println("\nSistema de Gestión para Estudiantes:");
-//    }
-//
-//    public static void main(String[] args) {
-//        System.out.println("Sistema de Matriculas de Estidiantes Intituto Tecnologico Superior Bolivar V0.10");
-//
-//    }
-//
-//=======
-//public static void main(String[] args) {
-//System.out.println("Sistema de Matriculas de Estidiantes Intituto Tecnologico Superior Bolivar V0.11");
-//       
-//Menu objetoMenu;
-//
-//objetoMenu.
-//}
-//>>>>>>> 432eb2e81dc8acc09dfdbcb55b33799700288d99
-//}
+package ec.edu.espe.registrationsystem.view;
+
+import java.awt.Menu;
+
+
+
+/**
+ *
+ * @author Marcelo Acuña, Logic Legion, DCCO-ESPE
+ */
+public class RegistrationSystem {
+public static void main(String[] args) {
+System.out.println("Sistema de Matriculas de Estidiantes Intituto Tecnologico Superior Bolivar V0.11");
+       
+Menu objetoMenu;
+
+objetoMenu.
+}
+}
