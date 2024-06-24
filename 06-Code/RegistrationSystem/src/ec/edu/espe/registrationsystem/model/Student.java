d49e864a8ff4558913e5e2249446328bb6da7525
adarboleda <adarboleda@espe.edu.ec>
2024-06-24 16:28:14 -0500

    Student

--- 06-Code/RegistrationSystem/src/ec/edu/espe/registrationsystem/model/Student.java
+++ 06-Code/RegistrationSystem/src/ec/edu/espe/registrationsystem/model/Student.java
@@ -14,12 +14,17 @@ public class Student {
     private String careerCode;
     private String email;
     private String phone;
-    private String typeOfRegistration;
+    private Registration typeOfRegistration;
     private String grade;
     private String lastChance;
     private String gratuity;
 
-    public Student(String dni, String name, String lastName, String careerCode, String email, String phone, String typeOfRegistration, String grade, String lastChance, String gratuity) {
+    @Override
+    public String toString() {
+        return "Student{" + "dni=" + dni + ", name=" + name + ", lastName=" + lastName + ", careerCode=" + careerCode + ", email=" + email + ", phone=" + phone + ", typeOfRegistration=" + typeOfRegistration + ", grade=" + grade + ", lastChance=" + lastChance + ", gratuity=" + gratuity + '}';
+    }
+
+    public Student(String dni, String name, String lastName, String careerCode, String email, String phone, Registration typeOfRegistration, String grade, String lastChance, String gratuity) {
         this.dni = dni;
         this.name = name;
         this.lastName = lastName;
@@ -32,89 +37,146 @@ public class Student {
         this.gratuity = gratuity;
     }
 
-    @Override
-    public String toString() {
-        return dni + "," + name + "," + lastName + "," + careerCode + "," + email + "," + phone + "," + typeOfRegistration + "," + grade + "," + lastChance + "," + gratuity;
-    }
-
+    /**
+     * @return the dni
+     */
     public String getDni() {
         return dni;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getCareerCode() {
-        return careerCode;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getTypeOfRegistration() {
-        return typeOfRegistration;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public String getLastChance() {
-        return lastChance;
-    }
-
+    /**
+     * @param dni the dni to set
+     */
     public void setDni(String dni) {
         this.dni = dni;
     }
 
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * @return the careerCode
+     */
+    public String getCareerCode() {
+        return careerCode;
+    }
+
+    /**
+     * @param careerCode the careerCode to set
+     */
     public void setCareerCode(String careerCode) {
         this.careerCode = careerCode;
     }
 
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
-    public void setTypeOfRegistration(String typeOfRegistration) {
+    /**
+     * @return the typeOfRegistration
+     */
+    public Registration getTypeOfRegistration() {
+        return typeOfRegistration;
+    }
+
+    /**
+     * @param typeOfRegistration the typeOfRegistration to set
+     */
+    public void setTypeOfRegistration(Registration typeOfRegistration) {
         this.typeOfRegistration = typeOfRegistration;
     }
 
+    /**
+     * @return the grade
+     */
+    public String getGrade() {
+        return grade;
+    }
+
+    /**
+     * @param grade the grade to set
+     */
     public void setGrade(String grade) {
         this.grade = grade;
     }
 
+    /**
+     * @return the lastChance
+     */
+    public String getLastChance() {
+        return lastChance;
+    }
+
+    /**
+     * @param lastChance the lastChance to set
+     */
     public void setLastChance(String lastChance) {
         this.lastChance = lastChance;
     }
 
+    /**
+     * @return the gratuity
+     */
     public String getGratuity() {
         return gratuity;
     }
 
+    /**
+     * @param gratuity the gratuity to set
+     */
     public void setGratuity(String gratuity) {
         this.gratuity = gratuity;
     }
+
+    
 
 }
