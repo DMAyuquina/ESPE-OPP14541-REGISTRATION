7ca176eb434424d4cb3da5e03b249f4bf0684662
adarboleda <adarboleda@espe.edu.ec>
2024-07-17 19:37:25 -0500

    frmAdmin

--- 06-Code/RegisterSystemMaven/src/main/java/ec/edu/espe/registersystemmaven/view/FrmTutor.java
+++ 06-Code/RegisterSystemMaven/src/main/java/ec/edu/espe/registersystemmaven/view/FrmTutor.java
@@ -24,6 +24,7 @@ public class FrmTutor extends javax.swing.JFrame {
     private void initComponents() {
 
         jMenuItem1 = new javax.swing.JMenuItem();
+        jPanel1 = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         mnuTutor = new javax.swing.JMenu();
         itmProfile = new javax.swing.JMenuItem();
@@ -42,6 +43,19 @@ public class FrmTutor extends javax.swing.JFrame {
         jMenuItem1.setText("jMenuItem1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(252, 252, 252));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 369, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 277, Short.MAX_VALUE)
+        );
 
         mnuTutor.setText("Tutor");
 
@@ -137,11 +151,11 @@ public class FrmTutor extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 369, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 277, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -233,6 +247,7 @@ public class FrmTutor extends javax.swing.JFrame {
     private javax.swing.JMenuItem itmUploadGrade;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JMenu mnuCourse;
     private javax.swing.JMenu mnuReport;
     private javax.swing.JMenu mnuTutor;
