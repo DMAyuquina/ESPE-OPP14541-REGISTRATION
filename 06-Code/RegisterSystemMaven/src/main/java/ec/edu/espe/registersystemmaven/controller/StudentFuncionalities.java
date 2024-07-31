bb78147fe635ba2b51e6124cc1d70fa576f20510
(ayuquina) <dmayuquina@espe.edu.ec>
2024-07-31 12:01:35 -0500

    s

--- 06-Code/RegisterSystemMaven/src/main/java/ec/edu/espe/registersystemmaven/controller/StudentFuncionalities.java
+++ 06-Code/RegisterSystemMaven/src/main/java/ec/edu/espe/registersystemmaven/controller/StudentFuncionalities.java
@@ -1,17 +1,18 @@
-
 package ec.edu.espe.registersystemmaven.controller;
 
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import ec.edu.espe.registersystemmaven.model.Student;
+import java.util.Scanner;
 import org.bson.Document;
+import utils.ValidationOfData;
 
 /**
  *
  * @author Danny Ayuquina, LogicLegion, DCCO-ESPE
  */
 public class StudentFuncionalities {
-        public static Student getStudent(MongoCollection<Document> mongoCollection, String key,String studentId) {
+
+    public static Student getStudent(MongoCollection<Document> mongoCollection, String key, String studentId) {
 
         Document findDocument = new Document(key, studentId);
 
@@ -45,4 +46,36 @@ public class StudentFuncionalities {
         return new Student(id, names, lastNanmes, genre, email, careerCode, phone, career, typeOfRegistration, u1, u2, lastChance);
     }
 
+    public void calculateFinalGrade(double unit1Grade, double unit2Grade, double supplementaryGrade) {
+
+        double finalGrade = (unit1Grade + unit2Grade + supplementaryGrade) / 3;
+
+        System.out.println("La calificación final del estudiante es: " + finalGrade);
+
+    }
+
+    public String calculateGrade() {
+
+        Scanner scanner = new Scanner(System.in);
+        String stringFinalGrade;
+
+        System.out.println("Ingrese notas del alumno:");
+        float unitOne = 0.0F;
+        do {
+            System.out.print("Unidad 1: ");
+            unitOne = ValidationOfData.validationOfFloat(unitOne, scanner);
+        } while (unitOne == -1.0F);
+
+        float unitTwo = 0.0F;
+        do {
+            System.out.print("Unidad 2: ");
+            unitTwo = ValidationOfData.validationOfFloat(unitTwo, scanner);
+        } while (unitTwo == -1.0F);
+        float finalGrade = (unitOne + unitTwo) / 2;
+
+        stringFinalGrade = String.valueOf(finalGrade);
+
+        return stringFinalGrade;
+
+    }
 }
